434812aff6aac7494794059eccab8b4f5a1e11a8
alessandro.colantoni <alessandro.colantoni@AKTPO011.aktios.lan>
2019-06-16 19:07:04 +0200

    distance and dto packages

---
+++ src/main/java/it/wcc/postcode/distance/controller/DistanceController.java
@@ -0,0 +1,10 @@
+package it.wcc.postcode.distance.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("distance/")
+public class DistanceController {
+
+}
